487ad7548cc8c688bcfdd4a0f62d4f2b547ffeba
Mrs Crypt <reokaro89@gmail.com>
2025-04-05 22:38:01 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.classList.toggle("dark");
   });
 });
+
+
+// Counter animation
+document.addEventListener("DOMContentLoaded", () => {
+  const counters = document.querySelectorAll(".counter");
+  counters.forEach(counter => {
+    counter.innerText = "0";
+    const updateCount = () => {
+      const target = +counter.getAttribute("data-target");
+      const count = +counter.innerText;
+      const increment = target / 100;
+
+      if (count < target) {
+        counter.innerText = Math.ceil(count + increment);
+        setTimeout(updateCount, 20);
+      } else {
+        counter.innerText = target;
+      }
+    };
+    updateCount();
+  });
+});
